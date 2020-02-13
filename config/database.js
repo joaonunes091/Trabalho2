e89afd111cd6bdba187f550f47023bac14766ddd
joaonunes091 <56521066+joaonunes091@users.noreply.github.com>
2020-02-13 18:04:44 +0000

    a

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/Livros';
+const mongoDB = 'mongodb://joaonunes091:Q9Ait1bDAkjElYjQRXUnkZ6xETVYqIEJhlLmSy2Pt7uQqAxbbMYWFa0KDsei33ll4woLmAaO2VmfvysNznxKYQ==@joaonunes091.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
